32ab95a3fa96897a2283118b6ef509dccf69e775
eid <eidhachem1@gmail.com>
2024-09-17 11:23:08 +0300

    Adjust import naming

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
-import counterSlice from "./reducers/counter-reducer"
-import authSlice from "./reducers/auth-reducer"
+import counterReducer from "./reducers/counter-reducer"
+import authReducer from "./reducers/auth-reducer"
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice,
-    auth: authSlice,
+    counter: counterReducer,
+    auth: authReducer,
   },
 })
 
