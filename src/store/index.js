683c49ddf01e582dcc06d726188e4cb67fccd66c
eid <eidhachem1@gmail.com>
2024-09-17 11:20:00 +0300

    Add auth slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import counterSlice from "./reducers/counter-reducer"
+import authSlice from "./reducers/auth-reducer"
 
 const store = configureStore({
   reducer: {
     counter: counterSlice,
+    auth: authSlice,
   },
 })
 
