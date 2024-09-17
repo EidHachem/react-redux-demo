8d8b6816a91db7a81bcf63f8d365d3312795d6dc
eid <eidhachem1@gmail.com>
2024-09-17 10:27:03 +0300

    Add new state value to control counter toggle

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css"
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector((state) => state.counter)
+  const showCounter = useSelector((state) => state.showCounter)
 
   const incrementHandler = () => {
     dispatch({ type: "increment" })
@@ -18,17 +19,21 @@ const Counter = () => {
     dispatch({ type: "decrement" })
   }
 
-  const toggleCounterHandler = () => {}
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={incrementByFiveHandler}>Increment by 5</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
+      {showCounter && (
+        <>
+          <div className={classes.value}>{counter}</div>
+          <button onClick={incrementHandler}>Increment</button>
+          <button onClick={incrementByFiveHandler}>Increment by 5</button>
+          <button onClick={decrementHandler}>Decrement</button>
+        </>
+      )}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   )
