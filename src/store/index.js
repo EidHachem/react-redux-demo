aeb38e6d0bca25b62efc79808149606bca94ee71
eid <eidhachem1@gmail.com>
2024-09-17 10:33:37 +0300

    Split redux code

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,5 @@
 import { createStore } from "redux"
-
-const initialState = { counter: 0, showCounter: true }
-
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      ...state,
-      counter: state.counter + 1,
-    }
-  }
-
-  if (action.type === "increase") {
-    return {
-      ...state,
-      counter: state.counter + action.payload,
-    }
-  }
-
-  if (action.type === "decrement") {
-    return {
-      ...state,
-      counter: state.counter - 1,
-    }
-  }
-
-  if (action.type === "toggle") {
-    return {
-      ...state,
-      showCounter: !state.showCounter,
-    }
-  }
-  return state
-}
+import counterReducer from "./reducers/counter-reducer"
 
 const store = createStore(counterReducer)
 
